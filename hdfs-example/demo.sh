90c07d05a4a5b42bdafc76e1a2fe532fbd652234
Matthew McCullough <matthewm@ambientideas.com>
2010-02-27 07:04:51 -0600

    Clean up filesystem before the HDFS demo

--- hdfs-example/demo.sh
+++ hdfs-example/demo.sh
@@ -1,3 +1,5 @@
+rm -rf output
+
 #Format the filesystem
 hadoop namenode -format
 
@@ -16,7 +18,7 @@ hadoop fs -put localinput/*xml* remoteinput
 hadoop fs -lsr remoteinput
 
 #Run the grep on all the files in localinput
-hadoop fs -mkdir output 
+#hadoop fs -mkdir output 
 hadoop jar /Applications/Dev/hadoop-family/hadoop-0.20.1/hadoop-*-examples.jar grep remoteinput output 'dfs[a-z.]+'
 
 #Look at the files remotely
@@ -24,4 +26,4 @@ hadoop fs -cat output/part*
 
 #pull the files local
 hadoop fs -get output localoutput
-cat localoutput/*
+cat localoutput/output/*
