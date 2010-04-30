68d5b382c30c04aba24cfb6f008ad315a1e6b20a
Matthew McCullough <matthewm@ambientideas.com>
2010-04-30 09:43:52 -0400

    Use log files instead of config files

--- hdfs-example/demo.sh
+++ hdfs-example/demo.sh
@@ -17,14 +17,14 @@ hadoop fs -rmr remoteinput
 
 #Upload files
 hadoop fs -mkdir remoteinput
-hadoop fs -put localinput/*xml* remoteinput
+hadoop fs -put localinput/*.log remoteinput
 
 #Check if the files are there
 hadoop fs -lsr remoteinput
 
 #Run the grep on all the files in localinput
 #hadoop fs -mkdir output 
-hadoop jar /Applications/Dev/hadoop-family/hadoop-0.20.1/hadoop-*-examples.jar grep remoteinput output 'dfs[a-z.]+'
+hadoop jar /Applications/Dev/hadoop-family/hadoop-0.20.1/hadoop-*-examples.jar grep remoteinput output '[a-z.]+html'
 
 #Look at the files remotely
 hadoop fs -cat output/part*
